0bb00d979d759305f1677c9ca97ff6edf36dbe93
Albérico Eduardo <albericoeduardo202@gmail.com>
2023-10-01 15:08:47 -0300

    StreamAPI

---
+++ src/StreamAPI/PredicateT/PredicateExample.java
@@ -0,0 +1,15 @@
+package StreamAPI.PredicateT;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Predicate;
+
+public class PredicateExample {
+    public static void main(String[] args) {
+        List<String> palavras = Arrays.asList("java","kotlin", "python", "javascript", "c", "go", "ruby");
+    
+        Predicate<String> maisDeCincoCaracteres = palavra -> palavra.length() >= 5;
+
+        palavras.stream().filter(maisDeCincoCaracteres).forEach(s -> System.out.println(s));
+    }
+}
